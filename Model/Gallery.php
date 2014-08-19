558d876f2aff970b28402bbf971eac413e22c7ea
David Herrmann <david@vworld.at>
2014-08-20 01:40:25 +0200

    Namespace fixes

--- Model/Gallery.php
+++ Model/Gallery.php
@@ -3,8 +3,8 @@
 namespace C33s\SimpleGalleryBundle\Model;
 
 use C33s\SimpleGalleryBundle\Model\om\BaseGallery;
-use c33s\AttachmentBundle\Collection\UploadCollection;
-use c33s\AttachmentBundle\Model\Attachment;
+use C33s\AttachmentBundle\Collection\UploadCollection;
+use C33s\AttachmentBundle\Model\Attachment;
 use Symfony\Component\Finder\SplFileInfo;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpFoundation\File\File;
@@ -13,7 +13,7 @@ class Gallery extends BaseGallery
 {
     protected $itemsCollection;
     protected $newImagesDirectory;
-    
+
     public function getItemsCollection()
     {
         if (null === $this->itemsCollection)
@@ -26,24 +26,24 @@ class Gallery extends BaseGallery
             $this->itemsCollection = new UploadCollection($items->getArrayCopy());
             $this->itemsCollection->setModel('C33s\\SimpleGalleryBundle\\Model\\GalleryItem');
         }
-        
+
         return $this->itemsCollection;
     }
-    
+
     public function setItemsCollection(UploadCollection $galleryItems)
     {
         return $this->setGalleryItems($galleryItems);
     }
-    
+
     public function getGalleryItems($criteria = null, \PropelPDO $con = null)
     {
         $c = GalleryItemQuery::create()
             ->orderBySortableRank()
         ;
-        
+
         return parent::getGalleryItems($c, $con);
     }
-    
+
     /**
      * Set images to load during postSave().
      *
@@ -54,17 +54,17 @@ class Gallery extends BaseGallery
     public function setImagesLoadFromDirectory($directory)
     {
         $this->newImagesDirectory = $directory;
-        
+
         return $this;
     }
-    
+
     /**
      * Process any attachment files that have been set but not attached yet.
      */
     protected function processNewUnsavedFiles(\PropelPDO $con = null)
     {
         parent::processNewUnsavedFiles($con);
-        
+
         if (null !== $this->newImagesDirectory)
         {
             $finder = Finder::create()
@@ -74,11 +74,11 @@ class Gallery extends BaseGallery
                 ->depth(0)
                 ->in($this->newImagesDirectory)
             ;
-            
+
             foreach ($finder as $file)
             {
                 $item = new GalleryItem();
-                
+
                 /* @var $file SplFileInfo */
                 $item
                     ->setTitle($file->getBasename('.'.$file->getExtension()))
@@ -86,12 +86,12 @@ class Gallery extends BaseGallery
                     ->setImageFilePath($file->getRealPath())
                 ;
             }
-            
+
             $this->newImagesDirectory = null;
-            
+
             $this->save($con);
         }
-        
+
         return $this;
     }
 }
